1843d8c799d1f52289da303bd8d75da8af05cd5f
Jacob Clark <jacob.clark@bbc.co.uk>
2015-06-18 12:46:48 +0100

    Removing logger

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());

ee74b67697381336c3d25a8691b0e539c9c3824f
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-06-19 00:11:54 +0100

    Removes unused require

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var bodyParser = require('body-parser');
 var exphbs  = require('express-handlebars');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 

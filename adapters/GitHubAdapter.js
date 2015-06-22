ad12e40f8ae4d163bdcd252ba256e5bc1b636bde
Jacob Clark <jacob.clark@bbc.co.uk>
2015-06-22 13:35:11 +0100

    Adds social icons, makes headers clickable

--- adapters/GitHubAdapter.js
+++ adapters/GitHubAdapter.js
@@ -4,7 +4,7 @@ var GitHubAdapter = function(){
     this.url = 'api.github.com'
     this.path = '/users/imjacobclark/events/public'
     this.username = 'imjacobclark'
-    console.log(process.env.GITHUB_TOKEN);
+
     this.headers = {
         'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36',
         'Authorization' : 'token ' + process.env.GITHUB_TOKEN

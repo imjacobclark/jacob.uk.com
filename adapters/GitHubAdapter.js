c9ad06aaa5553dc9385b391b7ec9c1291d7807dd
Jacob Clark <jacob.clark@bbc.co.uk>
2015-06-22 13:20:03 +0100

    Adds latest date and ability to authenticate with GitHub

--- adapters/GitHubAdapter.js
+++ adapters/GitHubAdapter.js
@@ -2,17 +2,25 @@ var utilities = require(__dirname + '/../lib/utilities');
 
 var GitHubAdapter = function(){
     this.url = 'api.github.com'
-    this.path = '/users/imjacobclark/events'
+    this.path = '/users/imjacobclark/events/public'
     this.username = 'imjacobclark'
+    console.log(process.env.GITHUB_TOKEN);
+    this.headers = {
+        'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36',
+        'Authorization' : 'token ' + process.env.GITHUB_TOKEN
+    };
 }
 
 GitHubAdapter.prototype.getProfileData = function(){
     return new Promise(function(resolve, reject){
-        utilities.doHTTPSRequest(this.url, this.path).then(function(data){
+        utilities.doHTTPSRequest(this.url, this.path, this.headers).then(function(data){
             var latestData = [];
             
             JSON.parse(data).forEach((data, i) => {
                 if(i >= 5) return
+
+                var date = new Date(data.created_at);
+                data.created_at = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
                 latestData.push(data);
             });
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var BlogAdapter = require(__dirname + '/../adapters/BlogAdapter')
 var GitHubAdapter = require(__dirname + '/../adapters/GitHubAdapter')
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var blogadapter = new BlogAdapter();
@@ -13,6 +14,7 @@ router.get('/', function(req, res, next) {
                 res.render('index', {posts: values[0], github: values[1]});
             }).catch(e => {
                 res.status(500);
+                console.log(e)
                 res.render('error');
             });
 });

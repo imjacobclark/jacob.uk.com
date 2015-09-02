6ea62abf82c010acaa025f0f609d8a5d32704287
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-09-02 09:31:52 +0100

    Removing GitHub from render

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,7 @@ var GitHubAdapter = require(__dirname + '/../adapters/GitHubAdapter')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var blogadapter = new BlogAdapter();
-    var githubadapter = new GitHubAdapter();
-
-    Promise.all([githubadapter.getProfileData(), githubadapter.getRepositories()])
-            .then(values => {
-                res.render('index', {github: values[1], repositories: values[2]});
-            }).catch(e => {
-                res.status(500);
-                console.log(e)
-                res.render('error');
-            });
+    res.render('index');
 });
 
 module.exports = router;

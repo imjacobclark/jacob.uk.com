8cac3025cc155c0c41c1b2da62763e68884451d7
Jacob Clark <jacob.jh.clark@googlemail.com>
2016-02-13 00:53:37 +0000

    Minify HTML

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var exphbs  = require('express-handlebars');
 var compression = require('compression');
+var minifyHTML = require('express-minify-html');
 
 var routes = require('./routes/index');
 
@@ -12,6 +13,19 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({}));
 app.set('view engine', 'handlebars');
+
+app.use(minifyHTML({
+    override:      true,
+    displayErrors: process.env.NODE_ENV === 'development',
+    htmlMinifier: {
+        removeComments:            true,
+        collapseWhitespace:        true,
+        collapseBooleanAttributes: true,
+        removeAttributeQuotes:     true,
+        removeEmptyAttributes:     true,
+        minifyJS:                  true
+    }
+}));
 
 app.use(compression());
 app.use(bodyParser.json());

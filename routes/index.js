1a8f1c1fe0c6ad134c8d8ad9eceec779adccfa15
Jacob Clark <jacob.jh.clark@googlemail.com>
2016-02-19 00:22:20 +0000

    Cache for a day

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var express = require('express'),
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    res.setHeader('Cache-Control', 'public, max-age=86400'))
     res.render('index');
 });
 

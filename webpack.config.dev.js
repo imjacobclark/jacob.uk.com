6ce58921203b4a77599598db599458f7ff3b930c
Jacob Clark <jacob.clark@bbc.co.uk>
2018-11-20 20:53:37 +0000

    Webpack chaneg

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,11 +3,9 @@ const path = require('path');
 module.exports = {
     entry: './index.js',
     mode: "production",
-    target: 'node',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js',
-        libraryTarget: 'commonjs2',
     },
     module: {
         rules: [

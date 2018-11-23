8d7c7db954a380d166d9b07dbc1fbe94e55ca5c2
Jacob Clark <jacob.clark@bbc.co.uk>
2018-11-23 08:18:28 +0000

    Listener tests

--- src/server/listener.js
+++ src/server/listener.js
@@ -1,6 +1,16 @@
 import app from '.'
 
+const DEFAULT_PORT = 3000;
+
+const getPort = process => {
+    if (!process.env)  {
+        return DEFAULT_PORT;
+    }
+
+    return process.env.PORT || DEFAULT_PORT
+}
+
 export default (appDependency = app, processDependency = process) => {
-    const port = processDependency.env.PORT || 3000;
+    const port = getPort(processDependency);
     appDependency.listen(port, () => console.log(`jacobclark.xyz is listening on port ${port}!`))
 };
--- test/server/listener.spec.js
+++ test/server/listener.spec.js
@@ -5,6 +5,20 @@ describe('Listener', () => {
         jest.restoreAllMocks();
     });
 
+    it('should spawn a new server on port 3000 when process.env is not defined', () => {
+        const mockApp = {
+            listen: jest.fn()
+        }
+
+        const mockProcess = {
+            "env": {}
+        }
+
+        listener(mockApp, mockProcess);
+
+        expect(mockApp.listen).toBeCalledWith(3000, expect.any(Function));
+    });
+
     it('should spawn a new server on port 3000 when process.env.PORT is not specified', () => {
         const mockApp = {
             listen: jest.fn()
@@ -33,5 +47,64 @@ describe('Listener', () => {
         listener(mockApp, mockProcess);
 
         expect(mockApp.listen).toBeCalledWith(8080, expect.any(Function));
+    });
+
+    it('should log out port 8080 when process.env.PORT is specified', (done) => {
+        const mockApp = {
+            listen: (port, callback) => {
+                jest.spyOn(global.console, 'log');
+
+                callback();
+
+                expect(console.log).toHaveBeenCalledWith("jacobclark.xyz is listening on port 8080!");
+                done();
+            } 
+        }
+
+        const mockProcess = {
+            "env": {
+                "PORT": 8080
+            }
+        }
+
+        listener(mockApp, mockProcess);
+    });
+
+    it('should log out port 3000 when process.env.PORT is not specified', (done) => {
+        const mockApp = {
+            listen: (port, callback) => {
+                jest.spyOn(global.console, 'log');
+
+                callback();
+
+                expect(console.log).toHaveBeenCalledWith("jacobclark.xyz is listening on port 3000!");
+                done();
+            }
+        }
+
+        const mockProcess = {
+            "env": {
+            }
+        }
+
+        listener(mockApp, mockProcess);
+    });
+
+    it('should log out port 3000 when process.env is not defined', (done) => {
+        const mockApp = {
+            listen: (port, callback) => {
+                jest.spyOn(global.console, 'log');
+
+                callback();
+
+                expect(console.log).toHaveBeenCalledWith("jacobclark.xyz is listening on port 3000!");
+                done();
+            }
+        }
+
+        const mockProcess = {
+        }
+
+        listener(mockApp, mockProcess);
     });
 });
